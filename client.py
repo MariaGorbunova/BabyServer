16bed2ed5cfbd4d430a5fcae49d27ba468dd89b7
Maria Gorbunova <maria.gorbunova@yahoo.com>
2021-03-17 10:18:53 -0700

    listens to several ports

--- client.py
+++ client.py
@@ -1,46 +1,71 @@
 #!/usr/bin/env python3
 # Maria Gorbunova
 
-# socket client demo
+# socket client receives server response
 
 import socket
+import sys
 
 HOST = '127.0.0.1'
-PORT = 5551
+# PORT = 5551
 
 
 class Client:
-    def __init__(self):
+    def __init__(self, port):
         with socket.socket() as s:
-            s.connect((HOST, PORT))
-            print("Client connect to:", HOST, "port:", PORT)
+            s.connect((HOST, port))
+            print("Client connect to:", HOST, "port:", port)
             validation = {'c': self.changeDir, 'f': self.newFile}
 
             mesg = 'd'
             s.send(mesg.encode('utf-8'))
             while mesg != 'q':
-
                 fromServer = s.recv(1024).decode('utf-8')
+                # if its the first time we get dir or changed dir we need to save this value
+                # print the directory and list of returned values
                 print("\nReceived from server:")  # , fromServer)
                 for serv_resp in fromServer.split(':'):
                     print(serv_resp)
 
-                mesg = ''
+                if mesg.split(':')[0] in list('dclfq'):
+                    self.directory = fromServer.split(':')[0]
+                    #print("Current directory for client on server: ", self.directory)
+
+                # validate users input
                 while mesg not in list('clfq'):
-                    print('\nMenu\nc: change to a new directory \nl: show all subdirectories and files of the current directory tree'
-                          '\nf: create a new file in the current directory\nq: quit')
+                    print(
+                        '\nMenu\nc: change to a new directory \nl: show all subdirectories and files of the current directory tree'
+                        '\nf: create a new file in the current directory\nq: quit')
                     mesg = input("Enter message to send or q to quit: ").strip()
 
-                if mesg in validation:
-                    mesg = validation[mesg]()
+                # mesg is user choice+currdir+aditional options
+                if mesg != 'q' and mesg != 'd':
+                    val = ''
+                    if mesg in validation:
+                        val = validation[mesg]()
+                    mesg += ':' + self.directory + ':' + val
+                    #print("In client to server:    ", mesg)
                 s.send(mesg.encode('utf-8'))
 
     def changeDir(self):
-        return 'c:' + input("Enter the name of the directory: ")
+        """returns input from the user for a directory"""
+        return input("Enter the name of the directory: ")
 
     def newFile(self):
-        return 'f:' + input("Enter the name of the new file: ")
+        """returns input from the user for a newfile"""
+        return input("Enter the name of the new file: ")
 
 
 if __name__ == '__main__':
-    c = Client()
+    try:
+        arg = int(sys.argv[1])
+        if len(sys.argv) == 2 and len(sys.argv[1]) == 4:
+            client = Client(arg)
+        else:
+            print("Command line argument is not valid!")
+    except TypeError as e:
+        print(str(e))
+        print("Enter an integer!")
+    except IndexError as e:
+        print(str(e))
+        print("Need a command line argument!")
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # Maria Gorbunova
 
-# socket server demo
+# socket server sends data to client
 
 import threading
 import socket
@@ -9,7 +9,7 @@ import sys
 import os
 
 HOST = "localhost"
-PORTS = [5551, 5552, 5553, 5554]
+PORT = 5551
 
 
 class Server:
@@ -17,85 +17,93 @@ class Server:
         threads = []
         for i in range(num):
             self.fromClient = ''
-            t = threading.Thread(target=self.start_socket,
-                                 args=(PORTS[i],))
+            self.curdir = ''
+            t = threading.Thread(target=self.start_socket, args=(PORT + i, num))
             threads.append(t)
             t.start()
         for t in threads:
             t.join()
 
-    def start_socket(self, port):
+    def start_socket(self, port, num):
         """ Creates a socket and listen for incoming messages"""
-        # The default socket_family is AF_INET, and the default socket_type is SOCK_STREAM,
+        # The default socket_family is AF_INET, and the default socket_type is SOCK_STREAM
         with socket.socket() as s:
             try:
                 s.settimeout(15)
                 s.bind((HOST, port))
                 print("Server hostname:", HOST, "port:", port)
 
-                s.listen()
+                s.listen(num)
                 (conn, addr) = s.accept()
+                # print(conn, addr)
                 while True:
+                    # (conn, addr) = s.accept()
                     self.fromClient = conn.recv(1024).decode('utf-8')
                     if self.fromClient == 'q':
                         break
                     print("From client:", addr)
                     print("Received:", self.fromClient)
-
                     mesg = self.menu()
+                    print("Sending to client:    ", mesg)
                     conn.send(mesg.encode('utf-8'))
             except socket.timeout:
                 print("Server says: TIMED OUT!")
-
         print(f"Socket on {port} is closed.")
 
     def menu(self):
+        """menu handles user menu options"""
         _menu = {'d': self.currentDir, 'c': self.changeDir, 'l': self.listAll, 'f': self.newFile}
         try:
-            # print(message)
+            # if its not the first time we change dir of the server to where each client is
+            if self.fromClient.split(':')[0] != 'd':
+                os.chdir(self.fromClient.split(':')[1])
+                self.curdir = os.getcwd()
             message = _menu[self.fromClient.split(':')[0]]()
         except KeyError as e:
             message = str(e)
-        return message
+        return self.curdir + ':' + message
 
     def currentDir(self):
-        print("In server CurrentDir:" + os.getcwd())
-        return os.getcwd()
+        """option d gets called only in the beginning of the program"""
+        self.curdir = os.getcwd()
+        return ''
 
     def changeDir(self):
+        """changes directory to the directory picked by user"""
+        message = ''
         try:
-            os.chdir(self.fromClient.split(':')[1])
-            message = 'Directory changed to:' + os.getcwd()
+            os.chdir(self.fromClient.split(':')[2])
+            self.curdir = os.getcwd()
+            message = "Changed directory"
         except Exception as e:
             message = str(e)
         return message
 
     def listAll(self):
-        print('List all files and directories: ')
-        print(os.getcwd())
-        message = os.getcwd()
+        """returns a string in a format of a list of files and directories in the curdir"""
+        # print('List all files and directories:', self.curdir)
+        message = ''
         if os.listdir():
             for i in os.listdir():
-                print(i)
                 message += ":" + i
         else:
-            message += ":Empty directory"
+            message = "Empty directory"
         return message
 
     def newFile(self):
-        # TODO:  print acknowledgment with the directory name
-        message = os.getcwd()
-        if not os.path.exists(self.fromClient.split(':')[1]):
-            with open(self.fromClient.split(':')[1], "w") as file:
+        """creates new file requested by user"""
+        print('In server creating a file...')
+        message = ''
+        if not os.path.exists(self.fromClient.split(':')[2]):
+            with open(self.fromClient.split(':')[2], "w") as file:
                 # file.write("Lab5")
-                message += ':File was created'
+                message = "File was created"
         else:
-            message = 'File already exists'
+            message = "Failed to create the file"
         return message
 
 
 if __name__ == '__main__':
-    
     try:
         arg = int(sys.argv[1])
         if len(sys.argv) == 2 and arg < 5:
@@ -105,5 +113,6 @@ if __name__ == '__main__':
     except TypeError as e:
         print(str(e))
         print("Enter an integer!")
-
-
+    except IndexError as e:
+        print(str(e))
+        print("Need a command line argument!")
